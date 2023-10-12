79136da668f2bd6be6558f173f531409471ad61e
Sassan Haradji <sassanh@gmail.com>
2023-10-12 18:46:34 +0400

    refactor: change Widgets with nested BoxLayouts to simple BoxLayouts

--- src/volume/__init__.py
+++ src/volume/__init__.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 import pathlib
 
 from constants import PRIMARY_COLOR, SECONDARY_COLOR
-from kivy.app import Builder, Widget
+from kivy.app import Builder
 from kivy.properties import ColorProperty, NumericProperty
+from kivy.uix.boxlayout import BoxLayout
 
 
-class VolumeWidget(Widget):
+class VolumeWidget(BoxLayout):
     value = NumericProperty(50)
     fill_color = ColorProperty(PRIMARY_COLOR)
     background_color = ColorProperty(SECONDARY_COLOR)
