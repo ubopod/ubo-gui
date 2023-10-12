d4efb2cec3f921524fde864faf9dcd16e943bd4e
Sassan Haradji <sassanh@gmail.com>
2023-10-12 18:44:08 +0400

    feat: add prompt widget

---
+++ src/prompt/__init__.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+import pathlib
+import warnings
+from typing import TYPE_CHECKING, Any
+
+from kivy.app import Builder
+from kivy.properties import ObjectProperty, StringProperty
+from page import PageWidget
+
+if TYPE_CHECKING:
+    from menu.types import Item
+
+PROMPT_OPTIONS = 2
+
+
+class PromptWidget(PageWidget):
+    icon = StringProperty()
+    title = StringProperty()
+    first_option_label = StringProperty()
+    first_option_icon = StringProperty()
+    first_option_callback = ObjectProperty()
+    second_option_label = StringProperty()
+    second_option_icon = StringProperty()
+    second_option_callback = ObjectProperty()
+
+    def __init__(
+        self: PromptWidget,
+        **kwargs: Any,  # noqa: ANN401
+    ) -> None:
+        super().__init__(
+            items=[
+                {
+                    'label': self.first_option_label,
+                    'icon': self.first_option_icon,
+                    'action': self.first_option_callback,
+                },
+                {
+                    'label': self.second_option_label,
+                    'icon': self.second_option_icon,
+                    'action': self.second_option_callback,
+                },
+            ],
+            **kwargs,
+        )
+
+    def get_item(self: PromptWidget, index: int) -> Item | None:
+        if not 1 <= index <= PROMPT_OPTIONS:
+            warnings.warn('index must be either 2 or 3',
+                          ResourceWarning, stacklevel=1)
+            return None
+        return self.items[index - 1]
+
+
+Builder.load_file(pathlib.Path(
+    __file__).parent.joinpath('prompt_widget.kv').resolve().as_posix())
