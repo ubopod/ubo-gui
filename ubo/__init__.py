30ac3af53a09e62972d90982445856ad581b3bde
Sassan Haradji <sassanh@gmail.com>
2023-10-14 14:37:14 +0400

    fix: add font assets to the package

--- ubo/__init__.py
+++ ubo/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-ROOT_PATH = Path(__file__).parent.parent
+ROOT_PATH = Path(__file__).parent
 ASSETS_PATH = ROOT_PATH.joinpath('assets')
 FONTS_PATH = ASSETS_PATH.joinpath('fonts')
