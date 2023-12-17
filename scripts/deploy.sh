09b65ecc99f7f57683487ce1c6e1af07379e6c94
Sassan Haradji <me@sassanh.com>
2023-12-18 00:28:26 +0400

    refactor: make prompt widget responsive to changes of its items

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e -o errexit
 
 poetry build
 
-LATEST_VERSION=$(basename $(ls -rt dist/*.whl | tail -n 1))
+LATEST_VERSION=$(basename $(ls -t dist/*.whl | head -n 1))
 
 scp dist/$LATEST_VERSION pi@ubo-development-pod:/tmp/
 ssh pi@ubo-development-pod "source ubo-gui/bin/activate && pip install --upgrade --force-reinstall --no-deps /tmp/$LATEST_VERSION && (killall demo-menu -9 || true) && demo-menu"
